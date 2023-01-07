65f24dededc039797124434b804cb357d2c2eecd
AbdelrahmanEsam <abdofahd20000@gmail.com>
2023-01-07 04:36:08 +0200

    Update Trie.kt

--- Trie.kt
+++ Trie.kt
@@ -1,44 +1,97 @@
-fun main() {
-    val input = readLine()!!.split(" ")
-    val root = Trie()
-   input.forEach {
-       root.insert(it)
-   }
-    val search = readLine()!!.split(" ")
-    search.forEach {
-        println(root.search(it))
-    }
-
-}
-
-
-class Trie {
-    val children : Array<Trie?> = Array(26){null}
-    var isEndOfWord = false
-}
-
-private fun Trie.insert(word : String)
-
+class Trie
 {
-    var index :Int
-    var currentNode = this
-    for (char in word){
-              index = char - 'a'
-        if (currentNode.children[index] == null)  currentNode.children[index] = Trie()
-        currentNode = currentNode.children[index]!!
-    }
-    currentNode.isEndOfWord = true
 
+    val root = TrieNode(null , null)
+
+    class  TrieNode (val data : Char? , val parent : TrieNode?)
+    {
+        val children : HashMap<Char,TrieNode?> = HashMap()
+        var isTerminating = false
+
+    }
 }
 
-private fun Trie.search(word: String) : Boolean
+fun Trie.insert(word : String)
 {
-    var index :Int
-    var currentNode = this
-    for (char in word){
-        index = char - 'a'
-        if (currentNode.children[index] == null)  return false
-        currentNode = currentNode.children[index]!!
+    var current = root
+
+    word.forEach {char ->
+        if ( current.children[char] == null)
+        {
+            current.children[char] = Trie.TrieNode(char, current)
+        }
+        current = current.children[char]!!
     }
-    return currentNode.isEndOfWord
+    current.isTerminating = true
+
+}
+fun Trie.insert(listOfWord : List<String>)
+{
+    listOfWord.forEach {
+        insert(it)
+    }
+}
+
+fun Trie.contains(word: String): Boolean {
+    var current = root
+    word.forEach {char ->
+        val child = current.children[char] ?: return false
+        current = child
+    }
+
+    return current.isTerminating
+}
+
+fun Trie.remove(word: String)
+{
+    var current = root
+
+    word.forEach { char ->
+        val child = current.children[char] ?: return
+        current = child
+    }
+
+    if (!current.isTerminating) return
+
+    current.isTerminating = false
+
+    val parent = current.parent
+
+    while (current.children.isEmpty() && !current.isTerminating)
+    {
+        parent?.let {
+         it.children[current.data!!] = null
+            current = it
+        }
+    }
+}
+
+fun Trie.getAllWordsWithPrefix(prefix : String) : List<String>
+{
+
+    var current = root
+
+    prefix.forEach { char ->
+        val child = current.children[char] ?: return  emptyList()
+        current = child
+    }
+
+    return current.getAllWordsWithThisPrefix(prefix)
+}
+
+fun Trie.TrieNode?.getAllWordsWithThisPrefix(prefix: String): List<String> {
+
+    val results = mutableListOf<String>()
+    if (this?.isTerminating == true)
+    {
+        results.add(prefix)
+    }
+
+    this?.children?.forEach { (char , node) ->
+
+        node?.let { results.addAll(it.getAllWordsWithThisPrefix(prefix+char)) }
+
+    }
+
+return results
 }
