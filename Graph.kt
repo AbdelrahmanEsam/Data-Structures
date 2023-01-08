d455a944fd45e45c2e3d9d7d7de81e72e201f0a3
AbdelrahmanEsam <abdofahd20000@gmail.com>
2023-01-08 06:59:05 +0200

    Update Graph.kt

--- Graph.kt
+++ Graph.kt
@@ -1,29 +1,80 @@
-private fun constructTheAdjacencyMatrixGraph(edges : Int,vertices:Int) : Array<IntArray>
+interface Graph<T>
 {
-    val adjMat = Array(vertices+1) { IntArray(vertices+1) }
-    repeat(edges)
-    {
-        val vertexOne = readInt()
-        val vertexTwo = readInt()
+    fun createVertex(data : T) : Vertex<T>
 
-        adjMat[vertexOne][vertexTwo] = 1
-//      adjMat[vertexTwo][vertexOne] = 1  if this graph is undirected
+    fun addDirectedEdge(source : Vertex<T>,destination: Vertex<T>,weight: Double?)
+
+
+    fun addUndirectedEdge(source : Vertex<T>,destination: Vertex<T>,weight: Double?)
+
+    fun add(edgeType: EdgeType,source : Vertex<T>,destination: Vertex<T>,weight: Double?)
+    {
+        when(edgeType)
+        {
+            EdgeType.DIRECTED -> {addDirectedEdge(source,destination,weight)}
+            EdgeType.UNDIRECTED -> {addUndirectedEdge(source, destination, weight)}
+        }
     }
-    return adjMat
+
+    fun edges(source:Vertex<T>) : ArrayList<Edge<T>>
+
+    fun weight(source: Vertex<T>, destination: Vertex<T>): Double?
+    
+    data class Vertex<T>(val index : Int ,val data :T)
+    data class Edge<T>(val source : Vertex<T>, val destination: Vertex<T>,val weight : Double? = null )
+    enum class EdgeType { DIRECTED, UNDIRECTED }
+    
 }
 
-private  fun constructTheAdjacencyListGraph(v:Int) : ArrayList<ArrayList<Int>>
-{
-    val adj = ArrayList<ArrayList<Int>>()
-    repeat(v)
-    {
-        adj.add(arrayListOf())
-    }
-    return adj
-}
 
-private fun ArrayList<ArrayList<Int>>.addEdge(firstEdge : Int, secondEdge :Int)
+
+
+
+
+
+class AdjacencyList<T> : Graph<T>
 {
-this[firstEdge].add(secondEdge)
-//  this[secondEdge].add(firstEdge) if this graph is undirected
+
+    private val neighbours : HashMap<Graph.Vertex<T>, ArrayList<Graph.Edge<T>>> = HashMap()
+
+
+    override fun createVertex(data: T): Graph.Vertex<T> {
+    val vertex =  Graph.Vertex(neighbours.count(), data)
+        neighbours[vertex] = ArrayList()
+        return vertex
+    }
+
+    override fun addDirectedEdge(source: Graph.Vertex<T>, destination: Graph.Vertex<T>, weight: Double?) {
+        neighbours[source]?.add(Graph.Edge(source,destination,weight))
+    }
+
+    override fun addUndirectedEdge(source: Graph.Vertex<T>, destination: Graph.Vertex<T>, weight: Double?) {
+       addDirectedEdge(source,destination,weight)
+        addDirectedEdge(destination,source,weight)
+    }
+
+    override fun add(edgeType: Graph.EdgeType,source: Graph.Vertex<T>, destination: Graph.Vertex<T>, weight: Double?) {
+       when(edgeType)
+       {
+           Graph.EdgeType.DIRECTED -> {addDirectedEdge(source,destination,weight)}
+           Graph.EdgeType.UNDIRECTED -> {addUndirectedEdge(source, destination, weight)}
+       }
+    }
+
+    override fun edges(source: Graph.Vertex<T>): ArrayList<Graph.Edge<T>> = neighbours[source] ?: arrayListOf()
+
+    override fun weight(source: Graph.Vertex<T>, destination: Graph.Vertex<T>): Double? {
+     return neighbours[source]?.firstOrNull { it.destination == destination }?.weight
+    }
+
+
+    override fun toString(): String {
+        return buildString { // 1
+            neighbours.forEach { (vertex, edges) ->
+                val edgeString = edges.joinToString {
+                    it.destination.data.toString() } // 3
+                append("${vertex.data} ---> [ $edgeString ]\n") // 4
+            }
+        }
+    }
 }
