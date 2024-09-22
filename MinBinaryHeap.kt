b4394da36735afd9e4c900a0f3b3f8eb01082c36
AbdelrahmanEsam <abdelrahmanesam20000@gmail.com>
2024-09-22 20:37:06 +0300

    Create MinBinaryHeap.kt

---
+++ MinBinaryHeap.kt
@@ -0,0 +1,45 @@
+// this class depends on the BinaryTree with arrayList Implementation
+
+
+class MinBinaryHeap<T : Comparable<T>> : BinaryTree<T>() {
+
+    override fun insertValue(value: T) {
+        super.insertValue(value)
+        siftUp(nodes.size - 1)
+    }
+
+
+    private fun siftUp(index: Int) {
+        var currentIndex = index
+        while (currentIndex > 0 && nodes[currentIndex] < nodes[getParentIndex(index)]) {
+            nodes[currentIndex] =
+                nodes[getParentIndex(index)].also { nodes[getParentIndex(index)] = nodes[currentIndex] }
+            currentIndex = getParentIndex(index)
+        }
+    }
+
+    private fun siftDown(index: Int) {
+        var currentIndex = index
+        while (getLiftChildIndex(currentIndex) < nodes.size) {
+            var smallestChildIndex = getLiftChildIndex(currentIndex)
+
+            if (getRightChildIndex(currentIndex) < nodes.size - 1
+                && smallestChildIndex < nodes.size - 1 &&
+                getItemByIndex(getRightChildIndex(currentIndex)) < getItemByIndex(smallestChildIndex)
+            ) {
+                smallestChildIndex = getRightChildIndex(currentIndex)
+            }
+            if (nodes[currentIndex] <= nodes[smallestChildIndex]) break // the current item is smaller than the child
+
+            nodes[currentIndex] = nodes[smallestChildIndex].also { nodes[smallestChildIndex] = nodes[currentIndex] }
+            currentIndex = smallestChildIndex
+        }
+    }
+
+    fun extractRoot(): T {
+        nodes[0] = nodes[nodes.size - 1].also { nodes[nodes.size - 1] = nodes[0] }
+        val extracted = nodes.removeLast()
+        siftDown(0)
+        return extracted
+    }
+}
